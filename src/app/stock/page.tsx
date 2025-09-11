9f7361cb09b714041ea41d60f383168c6ddf0f02
krak69 <clement.lacour@live.com>
2025-09-11 15:28:54 +0200

    Update page.tsx

--- src/app/stock/page.tsx
+++ src/app/stock/page.tsx
@@ -1,6 +1,6 @@
 'use client';
 import { useEffect, useState } from 'react';
-import { supabase } from '@/lib/supabase/client';
+import { supabase } from '../../lib/supabase/client';
 
 export default function StockPage(){
   const [rows, setRows] = useState<any[]>([]);
